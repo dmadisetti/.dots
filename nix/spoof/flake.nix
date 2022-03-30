27c36a86e769d64d660280e72a38e128238f0d29
dmadisetti <dylan.madisetti@gmail.com>
2022-03-30 19:44:12 +0000

    Deploying to template from @ dmadisetti/.dots@e1d75dd7354b7073b6536e2a46ff22ec12e8180b 🚀

---
+++ nix/spoof/flake.nix
@@ -0,0 +1,67 @@
+# Fantastic Fake Flake For Fooling Flake tests‽
+# 🥸 Should be valid nix, but also double as a mustache template.
+{
+  description = "{{user}}'s flake";
+
+  inputs = { };
+
+  outputs = inputs@{ self, ... }: {
+    lib = {
+
+      user = /*Default is dylan (hi) for testing reasons.{{#unless user}}*/ "dylan";
+      #{{else}}*/ "{{user}}";{{/unless}}
+      # Change this with `mkpasswd -m sha512crypt`.
+      hashed = "{{hashed}}";
+
+      default_wm = "{{default_wm}}";
+      networking = /*Networking 📡📡📡{{#unless networking}}*/ { };
+      #{{else}}*/{{{networking}}};{{/unless}}
+
+      keybase = {
+        enable = /*Only relevant for live images.{{#if keybase}}*/ true;
+        #{{else}}*/ false;{{/if}}
+        username = "{{keybase_username}}";
+        paper = ''{{{keybase_paper}}}'';
+      };
+      git = {
+        enable = /*User particular information.{{#if git}}*/ true;
+        #{{else}}*/ false;{{/if}}
+        name = "{{git_name}}";
+        email = "{{git_email}}";
+        signing = {
+          enable = /*Enforce signatures.{{#if git_signing}}*/ true;
+          #{{else}}*/ false;{{/if}}
+          key = "{{git_signing_key}}";
+        };
+      };
+      certificates = [
+        #{{{certificates}}}
+      ];
+
+      pkgs = [
+        #{{{pkgs}}}
+      ];
+
+      # Just a bunch of other stuff that is not as easily configurable.
+      #{{#unless misc}}
+      getty = pkgs_rev: dots_rev: ''
+        ▄▄▄▄▄▄▄   ▄ ▄▄ ▄  ▄▄▄ ▄▄▄▄▄▄▄  Linux \r (\m)
+        █ ▄▄▄ █ ▀ ▀ ▄█▀█▀   ▄ █ ▄▄▄ █  nixpkgs/${pkgs_rev}
+        █ ███ █ ▀█▀ ▀ ▀ █▀ ▄▄ █ ███ █  dots/${dots_rev}
+        █▄▄▄▄▄█ █▀▄▀█ ▄▀█▀█ █ █▄▄▄▄▄█  \d
+        ▄▄▄▄▄ ▄▄▄█▀█  ▀▄▀▄▀█▀▄ ▄ ▄ ▄
+        █▄▀██▀▄▄█ █ ▀██▄██▀▀██▀██▀▀▀▀
+        ▀▄█▄ █▄▄▀▄▄ █▀▄ ▀▄█▄▀ ▀ ▀█▄▄▄
+        ▀███ ▄▄█▄ ▄▄▄  ▄▀█▀▀▀ ▀▀█▀ ▀▀
+        ██▀ ▀▀▄█▀ ██  ▀▀▀▄▀ █▀ █▀▀▄▄
+        █▀▀ ▀█▄▀ ▀▄ ▀██▄█  ▀██ ▀▀██▀▀
+        █ ▄█▀▄▄ ▄ ▀ █▀▄ ▀▄▀▀▄█▄█▄▄▄ ▄
+        ▄▄▄▄▄▄▄ █▄ ▄▄  ▄▀ ▀██ ▄ █ █▀▀
+        █ ▄▄▄ █ ▄ ██  ▀ ▀▄ ▀█▄▄▄█▀▄▀
+        █ ███ █ █   ▀██▄▀▄███ ▄▄▄▄▄█▀
+        █▄▄▄▄▄█ █ ▄ █▀▄▀ ▄ ▄▀█ █ ▀▄▀
+      '';
+      #{{else}}{{{misc}}}{{/unless}}
+    };
+  };
+}
