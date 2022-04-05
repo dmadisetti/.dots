a623d4e565dc53219d68616d7f54620f7baf689b
dmadisetti <dylan.madisetti@gmail.com>
2022-04-05 21:00:59 +0000

    Deploying to template from @ dmadisetti/.dots@a2821b12a3d0218143275eace8a3ffad75816aef 🚀

---
+++ config/fish/functions/update.fish
@@ -0,0 +1,11 @@
+set DOTFILES ~/.dots
+function update
+    pushd $DOTFILES
+    set -l new (git ls-remote github:NixOs/nixpkgs refs/heads/master | cut -f1)
+    set -l old (+ jq -- jq ".nodes.nixpkgs.locked.rev" flake.lock | tr -d '"')
+    sed -i s/$old/$new/ flake.nix
+    unlock nixpkgs
+    nix flake update
+    unlock
+    popd
+end
