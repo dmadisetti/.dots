fea348ad2c22512337be32c4136c4271df0dbe97
dmadisetti <dylan.madisetti@gmail.com>
2022-04-06 05:09:12 +0000

    Deploying to template from @ dmadisetti/.dots@37fe2fff5a09f699b804f5f27e09b07b9a81ab9d 🚀

--- dot/config/fish/functions/snix.fish
+++ dot/config/fish/functions/snix.fish
@@ -3,7 +3,10 @@ function snix
   pushd $DOTFILES;
   mv ~/.xmonad/xmonad-x86_64-linux ~/.xmonad/xmonad-x86_64-linux.old 2> /dev/null;
   if test (count $argv) -eq 0
-    sudo nixos-rebuild switch -j auto --flake ".#"
+    sudo nixos-rebuild switch \
+      --override-input sensitive \
+      $DOTFILES/nix/sensitive \
+      -j auto --flake ".#"
   else
     sudo nixos-rebuild $argv[1..-1] -j auto --flake ".#"
   end
