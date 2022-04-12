92fce57d73b68fbba7f645c0818d4c9ecf53c230
dmadisetti <dylan.madisetti@gmail.com>
2022-04-12 17:35:53 +0000

    Deploying to template from @ dmadisetti/.dots@f4837419bacb933fc65f950bae37d534bbd8efc3 🚀

---
+++ nix/home/misc/eww.nix
@@ -0,0 +1,19 @@
+{ inputs, pkgs, home, ... }:
+let
+  weather =
+    if inputs.sensitive.lib ? weather && inputs.sensitive.lib.weather.enable
+    then inputs.sensitive.lib.weather
+    else { key = ""; city = ""; };
+in
+{
+  imports = [ ];
+
+  home.packages = with pkgs; [
+    eww # bars and widgets
+  ];
+
+  home.sessionVariables = {
+    WEATHER_KEY = weather.key;
+    WEATHER_CITY = weather.city;
+  };
+}
