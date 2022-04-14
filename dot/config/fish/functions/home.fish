bc03dc489247e0d79a3bc46fc6ea12fd4e11844d
dylan madisetti <madisetti@jhu.edu>
2022-04-14 13:38:23 +0000

    Use release branch for copilot

--- dot/config/fish/functions/home.fish
+++ dot/config/fish/functions/home.fish
@@ -2,12 +2,9 @@ function home
   pushd $DOTFILES;
   unlock
   if test (count $argv) -eq 0
-    home-manager switch \
-      --override-input sensitive \
-      $DOTFILES/nix/sensitive \
-      --flake ".#$USER" -j auto
+    nix run ".#home" -j auto
   else
-    home-manager $argv[1..-1] -j auto
+    + home-manager -- home-manager "$argv[1..-1]" -j auto
   end
   popd;
 end
