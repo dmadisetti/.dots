7da6935c109a0aba39942a659a490242113d7465
dmadisetti <dylan.madisetti@gmail.com>
2022-04-18 17:41:27 +0000

    Deploying to template from @ dmadisetti/.dots@1188631e7cfb5a19ceec7d943d75aa1f1615f725 🚀

--- scripts/create-home.nix.sh
+++ scripts/create-home.nix.sh
@@ -13,11 +13,43 @@ if [ ! -d $DOTFILES ]; then
   cd -
 fi
 
+infer_settings() {
+  local EXTRA=""
+  EXTRA="\"keybase\":{"
+  if [ -n "$KEYBASE_USER" ]; then
+    EXTRA="$EXTRA\"enable\":true"
+    EXTRA="$EXTRA, \"keybase_username\":\"$KEYBASE_USER\""
+    EXTRA="$EXTRA, \"keybase_paper\":\"$(cat /boot/paper* || echo '')\""
+  fi
+  EXTRA="$EXTRA}, \"git\": {\"enable\":true";
+  GIT_USER="$(git config user.name)"
+  if [ -n "$GIT_USER" ]; then
+    EXTRA="$EXTRA, \"git_name\":\"$GIT_USER\""
+  fi
+  GIT_EMAIL="$(git config user.email)"
+  if [ -n "$GIT_EMAIL" ]; then
+    EXTRA="$EXTRA, \"git_email\":\"$GIT_EMAIL\""
+  fi
+  if "true" = "$(git config commit.gpgsign)"; then
+    GIT_KEY="$(git config user.signingKey)"
+    if [ -n "$GIT_KEY" ]; then
+      EXTRA="$EXTRA, \"git_signing\":{"
+      EXTRA="$EXTRA\"enable\":true"
+      EXTRA="$EXTRA, \"git_signing_key\":\"$GIT_KEY\""
+      EXTRA="$EXTRA}";
+    fi
+  fi
+  EXTRA="$EXTRA}";
+  echo $EXTRA
+}
+
+EXTRA="$(infer_settings)"
 if [ ! -f $DOTFILES/nix/sensitive/flake.nix ]; then
   dots-manager template $SPOOF \
      $DOTFILES/nix/sensitive/flake.nix \
      <(echo "{\"user\": \"$USER\", \
               \"hashed\":\"\", \
+              $EXTRA, \
               \"networking\":\"{}\", \
               \"default_wm\":\"none\"}")
 fi
@@ -25,4 +57,5 @@ fi
 home-manager switch \
   --override-input sensitive \
   $DOTFILES/nix/sensitive \
+  --show-trace \
   --flake "$DOTFILES#$USER" -j auto
