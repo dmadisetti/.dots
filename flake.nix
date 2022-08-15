886a94bcf913066ec84afb3c3de525b8aa9687bd
dmadisetti <dylan.madisetti@gmail.com>
2022-08-15 14:46:04 +0000

    Deploying to template from @ dmadisetti/.dots@966151e88dc9e63b4ca276b002c725051e53b0e1 🚀

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     # This ensures that we always use the official nix cache.
     # nixpkgs.url = "/home/dylan/src/nixpkgs-local?cache-bust=4";
     # TODO: Change to patch system NixOs/nix/issues#3920
-    nixpkgs.url = github:nixos/nixpkgs/4bdf4169ad2896236895ca607a843f30c9680345;
+    nixpkgs.url = github:nixos/nixpkgs/e105167e98817ba9fe079c6c3c544c6ef188e276;
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
 
     # Really just to streamline deps.
