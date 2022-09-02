feb9014b2b75c07030bce937c5c949e927593a4a
dylan madisetti <madisetti@jhu.edu>
2022-09-02 14:41:42 -0400

    pr pdate

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
     # TODO: Fix grub2-themes so that it can use pngs.
 
     # Hyprland is **such** eye candy
-    hyprland.url = github:vaxerski/Hyprland/v0.11.1beta;
+    hyprland.url = github:vaxerski/Hyprland/v0.12.0beta;
     hyprland.inputs.nixpkgs.follows = "nixpkgs";
 
     # Cachix for caching!
