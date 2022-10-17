2ee93c6f29d0af3982ed3adce984707df5fd5fb1
dylan madisetti <dylan@madisetti.me>
2022-10-17 11:07:08 -0400

    Upgrade

--- dot/config/fish/functions/prs.fish
+++ dot/config/fish/functions/prs.fish
@@ -1,6 +1,6 @@
 function prs
   set -l gh (+ gh -- which gh)
   for patched in ($gh pr list --author "" --json url -q "(.[] | .url)");
-    git apply (curl -sL $patched.diff | psub);
+    git apply --3way (curl -sL $patched.diff | psub);
   end
 end
