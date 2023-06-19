ca7211f1bb1099378a4cd047f2e1f340e056c108
dylan madisetti <dylan@madisetti.me>
2023-06-19 12:22:39 -0400

    Automate rust prs

--- dot/config/fish/functions/prs.fish
+++ dot/config/fish/functions/prs.fish
@@ -1,6 +1,21 @@
 function prs
   set -l gh (+ gh -- which gh)
-  for patched in ($gh pr list --author "" --json url -q "(.[] | .url)");
-    git apply --3way (curl -sL $patched.diff | psub);
+  set -l query
+
+  if test -z $argv[1]
+    # If no label is provided, list all PRs without filtering by label
+    echo "Right"
+    set query "(.[] | .url)"
+    echo $query
+  else
+    # If a label is provided, filter PRs by that label
+    set -l label $argv[1]
+    set query "(.[] | select(.labels[] | .name == \"$label\") | .url)"
+  end
+  echo $argv[1]
+  echo $query
+  for url in ($gh pr list --author "" --json url,labels -q $query)
+    echo $url
+    git apply --3way (curl -sL $url.diff | psub);
   end
 end
