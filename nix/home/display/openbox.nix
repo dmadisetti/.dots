6b27c44bd84126437f64e35713b424fdfb816c84
dmadisetti <dylan.madisetti@gmail.com>
2023-06-27 03:24:25 +0000

    Deploying to template from @ dmadisetti/.dots@8a71d68f52cb7710cc1b8b21610655671d68cd65 🚀

--- nix/home/display/openbox.nix
+++ nix/home/display/openbox.nix
@@ -1,18 +1,7 @@
 # Boxxeee <3
 { pkgs, home, ... }: {
-  # imports = [ ../programs/polybar.nix ];
 
-  # services.xserver.windowManager.openbox = {
-  #   enable = true;
-  #   # extraConfig = builtins.readFile ../../../dot/config/i3/i3.config;
-  #   # Zero out default config because nix is annoyingly paternalistic.
-  #   config = rec {
-  #     bars = [ ];
-  #     keybindings = { };
-  #   };
-  # };
-
-  # home.packages = with pkgs; [ picom feh playerctl ];
+  home.packages = with pkgs; [ openbox ];
 
   home.file.".xinitrc".source = ../../../dot/config/openbox/xinitrc;
 }
