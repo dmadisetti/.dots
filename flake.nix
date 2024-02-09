a21b7c0db2e3e08289aa827b0e55eec7758b2d4e
dylan madisetti <dylan@madisetti.me>
2024-02-08 20:07:10 -0500

    Full hash is now live

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     # This ensures that we always use the official nix cache.
     # nixpkgs.url = "/home/user/src/nixpkgs-local?cache-bust=4";
     # TODO: Change to patch system NixOs/nix/issues#3920
-    nixpkgs.url = github:nixos/nixpkgs/f8e2ebd66d09;
+    nixpkgs.url = github:nixos/nixpkgs/f8e2ebd66d097614d51a56a755450d4ae1632df1;
     nixos-hardware.url = github:NixOS/nixos-hardware;
 
     # Really just to streamline deps.
