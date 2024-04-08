a80b2459710fbe86f5663f61a2f0fe402fcfc036
dmadisetti <dylan@madisetti.me>
2024-04-08 17:25:58 +0000

    Deploying to template from @ dmadisetti/.dots@db1224f17708bc5da80dfd876f267292003db1a5 🚀

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
     # This ensures that we always use the official nix cache.
     # nixpkgs.url = "/home/user/src/nixpkgs-local?cache-bust=4";
     # TODO: Change to patch system NixOs/nix/issues#3920
-    nixpkgs.url = github:nixos/nixpkgs/d8fe5e6c92d0d190646fb9f1056741a229980089;
+    nixpkgs.url = github:nixos/nixpkgs/ff0dbd94265ac470dda06a657d5fe49de93b4599;
     nixos-hardware.url = github:NixOS/nixos-hardware;
 
     # Really just to streamline deps.
@@ -72,7 +72,7 @@
     grub2-themes.inputs.nixpkgs.follows = "nixpkgs";
 
     # Hyprland is **such** eye candy
-    hyprland.url = github:hyprwm/Hyprland/v0.37.1;
+    hyprland.url = github:hyprwm/Hyprland/v0.38.1;
     hyprland.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.inputs.systems.follows = "systems";
 
