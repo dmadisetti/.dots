40dde102452d70412666c35e4582a0d962b78ea4
dylan <dylan@marimo.io>
2025-04-13 21:47:49 -0700

    ok that was just me being lazy

--- nix/utils.nix
+++ nix/utils.nix
@@ -59,7 +59,7 @@
           home = {
             inherit stateVersion username;
             # home if string contains linux, Users if darwin
-            homeDirectory = if lib.strings.hasInfix "linux" system then
+            homeDirectory = if nixpkgs.lib.strings.hasInfix "linux" system then
               "/home/${username}"
             else
               "/Users/${username}";
